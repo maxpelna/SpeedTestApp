6a5f97ab2f2bc615344df19a8bd161f2cbd37e51
Maksims Pelna <47249456+maxpelna@users.noreply.github.com>
2025-10-05 11:36:52 +0300

    Initial Commit

---
+++ SpeedTest/SpeedTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedTestApp.swift
+//  SpeedTest
+//
+//  Created by Maksims Pelna on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeedTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
